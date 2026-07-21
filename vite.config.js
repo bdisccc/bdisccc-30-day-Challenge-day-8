aaf2daaff323d10f01bf4b7e7e4348bacc0e01e8
bdisccc <105622710+bdisccc@users.noreply.github.com>
2026-07-21 13:45:00 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/bdisccc-30-day-Challenge-day-8/",
+  base: "/30-day-Challenge-day-8/",
 });
